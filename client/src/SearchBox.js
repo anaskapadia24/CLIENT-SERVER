42f2fe645b990b128261b2d76a13ad3bed5729c8
Anas Kapadia <anaskapadia101@gmail.com>
2022-07-14 12:41:06 +0530

    CLIENT

---
+++ client/src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const SearchBox = () =>
+{
+    return (
+        <input type ="search"
+        className="search"
+        placeholder={}
+        onChange={}
+        />
+    )
+}
+export default SearchBox;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// PATH: server/index.js
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -5,43 +6,40 @@ import cors from 'cors';
 import router from './routes/auth.js';
 import './models/user.js';
 
-const app = express(); //THE WHOLE SERVER
-const PORT = 5000; 
-app.use(express.json());
+const app = express();
+const PORT = 5000;
 
-//the port 5000 will work for database
-// MIDDLEWARE CORS BODY PARSER
-//Middileware is the function that invoke by express routing layer becasue we are using express using app
-
+app.use(express.json())
 app.use(router);
 
-const customMiddleware = (req, res,next)=>{
-    console.log("Middle ware executed!!!")
-    next() 
+const customMiddleware = (req,res,next) =>{
+    console.log("Middleware Executed!!!");
+    next();
 }
-// app.use(customMiddleware)
-//USING GET METHOD INSTEAD OF POST
+//app.use(customMiddleware);
 app.get("/home",(req,res)=>{
-    console.log("hellow from localhost")
-    res.send("HELLO I AM RESPONSE FROM SERVER") //it will show on browser
+    console.log('Hello from Home Page'); //Terminal
+    res.send("I am Home Page"); //Browser
 })
 app.get("/login",customMiddleware,(req,res)=>{
-    console.log("hello from login page")
-    res.send("HELLO I AM RESPONSE FROM SERVER") //it will show on browser
-})
-app.listen(PORT,()=>{ //which port number should response go and what task should it perform after listening on the port
-    console.log("server is running on:",PORT)//it will show on terminal
-})  
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }))  //setting the limit for body of html by encoding like images should not be greater then 30mb 
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })) //for encoding the url  
-app.use(cors()); //timestamp function that tells from where the request has send or come
-const CONNECTION_URL = "mongodb+srv://socialnetwork:socialnetwork@cluster0.zmeh4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"  // connection of the mongodb database
-// And we have use nodemon for refreshment of the page automatically so nodemon will execute 
-//it automatically by updating the app and error and if error will occure it will show in terminal
-mongoose.connect(CONNECTION_URL).then(() => {  //connecting to mongodatabase
-    console.log(`server Running on ${PORT}`)
+    console.log('Hello from Login Page'); //Terminal
+    res.send("I am Login Page") //Browser
 })
 
-//Get Method: data goes to the url and it will show on url like https://www.google.com/data= ashok   get is faster than post
-//Post Method: post will not show the data on the url like https://www.google.com post is slower than get
+app.listen(PORT,()=>{
+    console.log("SERVER RUNNING ON:",PORT) //Terminal
+})
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://aksocialnetwork:aksocialnetwork@cluster0.ai3cl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(CONNECTION_URL).then(()=>{
+    console.log(`SERVER connect DATABASE through: ${PORT}`)
+})
+
+
+
+
